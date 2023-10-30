eb0dffb677e2740e510739514fcb4b6f798dd6d6
Juan2k-1 <jadova16@gmail.com>
2023-10-30 16:46:06 +0100

    Añadidas correcciones a la salida de los datos

--- src/main/java/juan/motores_busqueda/practica2_mb/ParserQueries.java
+++ src/main/java/juan/motores_busqueda/practica2_mb/ParserQueries.java
@@ -17,16 +17,19 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
-public class ParserQueries {
+public class ParserQueries
+{
 
-    private static String extractWordsFromLine(BufferedReader reader) throws IOException {
-        StringBuilder queryTextBuilder = new StringBuilder();
+    private static String extractWordsFromLine(BufferedReader reader, StringBuilder queryTextBuilder) throws IOException
+    {
         Pattern wordPattern = Pattern.compile("\\w+");
 
         String line;
-        while ((line = reader.readLine()) != null && !line.startsWith(".")) {
+        while ((line = reader.readLine()) != null && !line.startsWith("."))
+        {
             Matcher matcher = wordPattern.matcher(line);
-            while (matcher.find() && queryTextBuilder.length() < 5) {
+            while (matcher.find() && queryTextBuilder.length() < 5)
+            {
                 queryTextBuilder.append(matcher.group()).append(" ");
             }
         }
@@ -37,7 +40,8 @@ public class ParserQueries {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Por favor, proporciona la ruta del archivo CISI.QRY como argumento.");
         String filePath = scanner.nextLine();
@@ -47,32 +51,44 @@ public class ParserQueries {
         String solrUrl = "http://localhost:8983/solr/CORPUS2";
         HttpSolrClient solrClient = new HttpSolrClient.Builder(solrUrl).build();
 
-        try ( BufferedReader br = Files.newBufferedReader(pathToDocument.toAbsolutePath())) {
+        try ( BufferedReader br = Files.newBufferedReader(pathToDocument.toAbsolutePath()))
+        {
             String line;
             StringBuilder queryTextBuilder = new StringBuilder();
 
-            while ((line = br.readLine()) != null) {
-                if (line.startsWith(".I")) {
+            while ((line = br.readLine()) != null)
+            {
+                if (line.startsWith(".I"))
+                {
                     // No necesitamos la ID 
-                } else if (line.startsWith(".W")) {
-                    String queryText = extractWordsFromLine(br);
+                } else if (line.startsWith(".W"))
+                {
+                    String queryText = extractWordsFromLine(br, queryTextBuilder);
 
                     // Construir la consulta a Solr
                     SolrQuery solrQuery = new SolrQuery();
                     solrQuery.setQuery("content:" + queryText);
+                    solrQuery.set("fl", "id,title,score");
 
                     // Realizar la consulta a Solr y procesar los resultados
                     QueryResponse response = solrClient.query(solrQuery);
 
                     // Procesar los resultados de Solr
                     SolrDocumentList docs = response.getResults();
-                    for (int i = 0; i < docs.size(); ++i) {
+                    for (int i = 0; i < docs.size(); ++i)
+                    {
                         SolrDocument doc = docs.get(i);
                         String id = (String) doc.getFieldValue("id");
                         String title = (String) doc.getFieldValue("title");
+                        Float score = (Float) doc.getFieldValue("score");
+
                         System.out.println("ID: " + id);
                         System.out.println("Title: " + title);
+                        System.out.println("Score: " + score); // Mostrar el score
                         System.out.println("--------------------");
+
+                        /*System.out.println(docs.get(i));
+                        System.out.println("--------------------");*/
                     }
                     // Limpiar el StringBuilder para la próxima consulta
                     queryTextBuilder.setLength(0);
@@ -80,7 +96,8 @@ public class ParserQueries {
             }
             solrClient.close();
             scanner.close();
-        } catch (IOException | SolrServerException e) {
+        } catch (IOException | SolrServerException e)
+        {
             Logger.getLogger(ParserQueries.class.getName()).log(Level.SEVERE, e.getMessage(), e);
         }
     }
